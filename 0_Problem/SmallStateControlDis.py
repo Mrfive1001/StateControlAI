d5cf94da277b9b60b919929002b221343fc9a0f2
MrFive <13070156190@163.com>
2018-01-26 22:05:03 +0800

    将离散环境整理为连续环境的子类,学习了调用父类的方法和父类初始化

--- 0_Problem/SmallStateControlDis.py
+++ 0_Problem/SmallStateControlDis.py
@@ -1,12 +1,13 @@
 import numpy as np
 from SmallStateControl import SSCPENV as SSP
 
-# 连雪、、
+
+# 继承连续
 
 class SSCPENV(SSP):
     def __init__(self, x_dim=2, action_dim=1, init_x=None):
-        SSP.__init__(x_dim=2, action_dim=1, init_x=None)
-        self.abound = np.linspace(0,12,5)
+        super().__init__(x_dim, action_dim, init_x)
+        self.abound = np.linspace(0, 12, 10)
         self.n_action = len(self.abound)
 
     def step(self, omega):
@@ -14,4 +15,4 @@ class SSCPENV(SSP):
             if type(omega) == np.ndarray:
                 omega = omega[0]
         omega = self.abound[int(omega)]
-        return SSP.step(omega)
+        return super().step(omega)
