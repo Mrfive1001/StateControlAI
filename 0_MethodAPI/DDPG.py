77c51a26110366796af5290a9b640af858736348
MrFive <13070156190@163.com>
2018-01-27 19:25:19 +0800

    DDPG整理了格式，接下来编写A3C程序

--- 0_MethodAPI/DDPG.py
+++ 0_MethodAPI/DDPG.py
@@ -9,34 +9,35 @@ import sys
 
 tf.set_random_seed(2)
 
+
 ###############################  DDPG  ####################################
 
 class ddpg(object):
     def __init__(
             self,
-            a_dim,   # 动作的维度
-            s_dim,   # 状态的维度
-            a_bound, # 动作的上下限
+            a_dim,  # 动作的维度
+            s_dim,  # 状态的维度
+            a_bound,  # 动作的上下限
             e_greedy_end=0.1,  # 最后的探索值 e_greedy*幅值
             e_liner_times=1000,  # 探索值经历多少次学习变成e_end
-            epilon_init = 1,  # 表示1倍的幅值作为初始值
-            LR_A = 0.0001, # Actor的学习率
-            LR_C = 0.0002, # Critic的学习率
+            epilon_init=1,  # 表示1倍的幅值作为初始值
+            LR_A=0.0001,  # Actor的学习率
+            LR_C=0.0002,  # Critic的学习率
             GAMMA=0.9,  # reward discount
-            TAU = 0.01, # 软替代率，例如0.01表示学习eval网络0.01的值，和原网络0.99的值
-            MEMORY_SIZE = 10000,  # 记忆池容量
-            BATCH_SIZE = 256,   # 批次数量
-            units = 64,
+            TAU=0.01,  # 软替代率，例如0.01表示学习eval网络0.01的值，和原网络0.99的值
+            MEMORY_SIZE=10000,  # 记忆池容量
+            BATCH_SIZE=256,  # 批次数量
+            units=64,
             reload_flag=False,  # 是否读取
-            train = True  # 训练的时候有探索
-            ):
+            train=True  # 训练的时候有探索
+    ):
 
         # DDPG网络参数
         self.method = 'MovFan'
-        self.LR_A = LR_A    # learning rate for actor
-        self.LR_C = LR_C    # learning rate for critic
-        self.GAMMA = GAMMA    # reward discount
-        self.TAU = TAU      # soft replacement
+        self.LR_A = LR_A  # learning rate for actor
+        self.LR_C = LR_C  # learning rate for critic
+        self.GAMMA = GAMMA  # reward discount
+        self.TAU = TAU  # soft replacement
         self.MEMORY_CAPACITY = MEMORY_SIZE
         self.BATCH_SIZE = BATCH_SIZE
         self.units = units
@@ -49,12 +50,11 @@ class ddpg(object):
 
         self.pointer = 0
         self.a_replace_counter, self.c_replace_counter = 0, 0
-        self.iteration=0
+        self.iteration = 0
         self.modelpath = sys.path[0] + '/data.chkp'
 
-
         # DDPG构建
-        self.memory = np.zeros((self.MEMORY_CAPACITY, s_dim * 2 + a_dim + 1 + 1), dtype=np.float32) # 存储s,a,r,s_,done
+        self.memory = np.zeros((self.MEMORY_CAPACITY, s_dim * 2 + a_dim + 1 + 1), dtype=np.float32)  # 存储s,a,r,s_,done
         self.sess = tf.Session()
 
         self.a_dim, self.s_dim, self.a_bound = a_dim, s_dim, a_bound,
@@ -73,7 +73,6 @@ class ddpg(object):
             q_ = self._build_c(self.S_, a_, scope='target', trainable=False)
             self.ce_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/eval')
             self.ct_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/target')
-
 
         # target net replacement
         self.soft_replace = [
@@ -94,12 +93,10 @@ class ddpg(object):
         else:
             self.sess.run(tf.global_variables_initializer())
 
-
     def choose_action(self, s):
         action = self.sess.run(self.a, {self.S: s[np.newaxis, :]})[0]
-        return np.clip(np.random.normal(action, abs(self.a_bound[0]-self.a_bound[1])/2*self.epsilon),
-                       self.a_bound[0], self.a_bound[1]) # add randomness to action selection for exploration
-
+        return np.clip(np.random.normal(action, abs(self.a_bound[0] - self.a_bound[1]) / 2 * self.epsilon),
+                       self.a_bound[0], self.a_bound[1])  # add randomness to action selection for exploration
 
     def learn(self):
         if self.pointer < self.MEMORY_CAPACITY:
@@ -134,8 +131,7 @@ class ddpg(object):
             n_l1 = self.units
             net = tf.layers.dense(s, n_l1, activation=tf.nn.relu, name='l1', trainable=trainable)
             a = tf.layers.dense(net, self.a_dim, activation=tf.nn.tanh, name='a', trainable=trainable)
-            return tf.multiply(a, abs(self.a_bound[0]-self.a_bound[1])/2, name='scaled_a') + np.mean(self.a_bound)
-
+            return tf.multiply(a, abs(self.a_bound[0] - self.a_bound[1]) / 2, name='scaled_a') + np.mean(self.a_bound)
 
     def _build_c(self, s, a, scope, trainable):
         with tf.variable_scope(scope):
@@ -149,4 +145,3 @@ class ddpg(object):
 
     def net_save(self):
         self.actor_saver.save(self.sess, self.modelpath)
-
